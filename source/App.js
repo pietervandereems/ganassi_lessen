5125f6b8beeb4dbecdc0625ec0dc06d3c373dc3f
Pieter van der Eems <ganassi_github@eemco.nl>
2012-09-09 14:09:02 +0200

    Tablet Layout first setup

--- source/App.js
+++ source/App.js
@@ -1,10 +1,11 @@
 enyo.kind({
-	name: "App",
-	fit: true,
-	components:[
-		{name: "hello", content: "Hello World", allowHtml: true, ontap: "helloWorldTap"}
-	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.hello.addContent("<br/><b>hello</b> control was tapped");
-	}
+    name: "App",
+    kind: "Panels",
+    fit: true,
+    classes: "app-panels",
+    arrangerKind: "CollapsingArranger",
+    components: [
+        {name: "List"},
+        {name: "MyMiddlePanel"}
+    ]
 });
